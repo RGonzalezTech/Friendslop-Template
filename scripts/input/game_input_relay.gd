a95a6ab2dc236cb7a338622f1f98b32f5bd7c78c
Raul A. Gonzalez <info@rgonzalez.tech>
2026-01-01 00:32:47 -0800

    Add Moving Player

---
+++ scripts/input/game_input_relay.gd
@@ -0,0 +1,10 @@
+extends InputRelay
+class_name GameInputRelay
+
+func _init() -> void:
+    actions_to_monitor = [
+        "move_left",
+        "move_right",
+        "move_up",
+        "move_down",
+    ]
